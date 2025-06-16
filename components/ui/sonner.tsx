d1e4f2024b5342de2662da23251e66608cf705ac
firas Bentaleb <firasbt20182@gmail.com>
2025-06-16 18:26:53 +0100

    #edit

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -8,19 +8,6 @@ import { AlertTriangle, Info, CheckCircle, XCircle } from "lucide-react"; // Imp
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
 
-  React.useEffect(() => {
-    toast.success("Course created successfully!", { icon: <CheckCircle className="h-4 w-4 text-white" /> });
-    setTimeout(() => toast.error("Error creating course.", { icon: <XCircle className="h-4 w-4 text-white" /> }), 1000);
-    setTimeout(() => toast.info("New update available.", { icon: <Info className="h-4 w-4 text-gray-800" /> }), 2000);
-    setTimeout(() => toast.warning("Low course limit!", {
-      icon: <AlertTriangle className="h-4 w-4 text-yellow-800" />,
-      action: {
-        label: "Upgrade Now",
-        onClick: () => window.location.href = "/pricing",
-      },
-    }), 3000);
-  }, []);
-
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
